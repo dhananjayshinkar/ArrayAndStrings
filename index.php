90f0b464670e07ebec39a5c353f08809965f82ef
Dhananjay P Shinkar <dps48@afsconnect1.njit.edu>
2017-09-25 21:49:34 -0400

    Added array count function

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
  $chunk_array = array('a', 'b', 'c', 'd', 'e');
  $obj->splitArray($chunk_array);
 
+ $array_count = array(1, "hello", 1, "world", "hello");
+ $obj->countArray($array_count);
+
  class main
  {	
  	public function __construct()
@@ -45,6 +48,13 @@
 	{
 		echo '<h1>Array Chunk Function</h1>';
 		print_r(array_chunk($chunk_array, 2, true));
+		echo '<hr>';
+	}
+
+	public function countArray($array_count)
+	{
+		echo '<h1>Array Count Function</h1>';
+		print_r(array_count_values($array_count));
 		echo '<hr>';
 	}
 
